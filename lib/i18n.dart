83d5b4801d52c9508a25fd0d5b4d63614ad256ff
Richard Jansen <richard@pedroja.tech>
2024-02-04 20:09:02 +0100

    class methods

--- lib/i18n.dart
+++ lib/i18n.dart
@@ -1,3 +1,14 @@
 abstract class My24i18n {
   final String basePath = "generic";
+  static tr(String path, {Map<String, String>? namedArgs}) {}
+  String $trans(String key, {Map<String, String>? namedArgs, String? pathOverride}) {
+    if (key.split('.').isNotEmpty) {
+      return tr(key, namedArgs: namedArgs);
+    }
+    if (pathOverride != null) {
+      return tr("$pathOverride.$key", namedArgs: namedArgs);
+    }
+
+    return tr("$basePath.$key", namedArgs: namedArgs);
+  }
 }
